909826a084e9ecf7f100b1cb3a82822f75ce1170
tanchaowen84 <tanchaowen84@gmail.com>
2025-10-29 18:55:48 +0800

    feat:update database

---
+++ src/db/migrations/0004_brainy_sinister_six.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "logo_task" ADD COLUMN "rendered_video_key" text;--> statement-breakpoint
+ALTER TABLE "logo_task" ADD COLUMN "rendered_video_url" text;--> statement-breakpoint
+ALTER TABLE "logo_task" ADD COLUMN "rendered_at" timestamp;
